a0599999bce5ffadbf099d27c882284d0f6ff2c5
haomingjue <haomingjue@outlook.com>
2022-08-14 02:12:17 -0400

    Best Time to Buy and Sell Stocks

---
+++ src/array/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,16 @@
+package array;
+
+public class BestTimeToBuyAndSellStocks {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            }
+            else {
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
