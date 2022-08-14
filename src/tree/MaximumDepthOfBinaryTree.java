472c644e8251756d7edaed211d09911e0a136df7
haomingjue <haomingjue@outlook.com>
2022-08-14 01:57:30 -0400

    Maximum Depth of Binary Tree

---
+++ src/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,9 @@
+package tree;
+
+import utils.TreeNode;
+
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
