32f52f6d6e088b417513ab07a5d40f516db83ce3
haomingjue <haomingjue@outlook.com>
2022-08-14 22:25:53 -0400

    coins change dp bottom up

---
+++ src/dynamic_programming/CoinChange_DPBottomUp.java
@@ -0,0 +1,17 @@
+package dynamic_programming;
+
+import java.util.Arrays;
+
+public class CoinChange_DPBottomUp {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
