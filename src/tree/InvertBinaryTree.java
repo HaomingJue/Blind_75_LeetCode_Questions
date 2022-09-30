386a2975716497704fedd3af43f46fab62885e24
haomingjue <haomingjue@outlook.com>
2022-09-29 22:51:36 -0400

    feat: invert binary tree

---
+++ src/tree/InvertBinaryTree.java
@@ -0,0 +1,15 @@
+package tree;
+
+import utils.TreeNode;
+
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
