69fc5c730f8312f372d6400f5f7a7dc6706cefb6
haomingjue <haomingjue@outlook.com>
2022-10-20 23:41:26 -0400

    feat: lowest common ancestor of a binary search tree

---
+++ src/tree/LowestCommonAncestorOfABInarySearchTree.java
@@ -0,0 +1,20 @@
+package tree;
+
+import utils.TreeNode;
+
+public class LowestCommonAncestorOfABInarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
