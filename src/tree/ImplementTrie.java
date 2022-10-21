f4c9745400798c6a0ce3e7b84bbf52f7976c7ac9
haomingjue <haomingjue@outlook.com>
2022-10-21 14:42:16 -0400

    feat: implement trie

---
+++ src/tree/ImplementTrie.java
@@ -0,0 +1,48 @@
+package tree;
+
+import java.util.HashMap;
+
+public class ImplementTrie {
+    private TrieNode root;
+    private class TrieNode {
+        public HashMap<Character, TrieNode> children = new HashMap<>();
+        boolean isEnd = false;
+    }
+
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
+            }
+            node = node.children.get(c);
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return node.isEnd == true;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return true;
+    }
+}
